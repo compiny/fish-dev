3c407f81492cec9f40d625e97ad5efcf5019339f
User <info@compiny.ru>
2022-04-23 16:30:42 +0700

    Modules Vendors с глобальными State, getters

---
+++ app/Models/MetaDev.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Ramsey\Collection\Collection;
+
+class MetaDev extends Model
+{
+    use HasFactory;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    private static function vendor():array
+    {
+        return Vendor::all()->toArray();
+    }
+
+    private static function troubles():array
+    {
+        return Trouble::all()->toArray();
+    }
+
+    private static function types():array
+    {
+        return Type::all()->toArray();
+    }
+
+    static function factory()
+    {
+        $arr = [
+            'vendors' => self::vendor(),
+            'troublres' => self::troubles()
+        ];
+        return $arr;
+    }
+}
