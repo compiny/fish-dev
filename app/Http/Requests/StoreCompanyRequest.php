4dbb4176f1842899eef603031f1f6839543730e4
User <info@compiny.ru>
2022-05-10 14:48:01 +0700

    Request Company, User

---
+++ app/Http/Requests/StoreCompanyRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCompanyRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|unique:companies',
+            'nameOff' => 'required|unique:companies',
+            'inn' => 'required|unique:companies',
+            'kpp' => 'required|unique:companies',
+            'ogrn' => 'required|unique:companies',
+            'phones' => 'nullable',
+            'urAdr' => 'nullable',
+            'offAdr' => 'nullable',
+            'mailAdr' => 'nullable|email',
+            'buhID' => 'required',
+            'dirID' => 'required',
+        ];
+    }
+}
