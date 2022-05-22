81830eea63d80d9a1985b1c113c2f75ae5bd4e20
User <info@compiny.ru>
2022-05-22 21:17:56 +0700

    Request Dev

---
+++ app/Http/Requests/UpdateDevRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateDevRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'n' => 'required',
+            'date' => 'required',
+            'final' => 'nullable',
+            'troubles' => 'nullable',
+            'address' => 'nullable',
+            'phone' => 'nullable',
+            'customer_id' => 'required',
+            'type_id' => 'nullable',
+            'vendor_id' => 'nullable',
+            'sn' => 'nullable',
+            'notification' => 'boolean',
+        ];
+    }
+}
